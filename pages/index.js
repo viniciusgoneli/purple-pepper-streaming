5765a1c3cb1698ce5e53f3b7d90d7a4c11971b97
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-03-27 17:13:27 -0300

    Set 'Head' element

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
 import Head from 'next/head'
 
 export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Purple pepper</title>
+        <meta name="description" content="Watch all your favorite movies and series. Our cataloge is full." />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+    </>
+  )
 }
