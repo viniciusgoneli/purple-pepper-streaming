62358133464e961a3fd4b6018c9af98478b7f77d
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-03-29 08:38:14 -0300

    Created 'MoviesCatalogue' and 'MoviesList' components

---
+++ components/MoviesCatologue/MoviesCatalogue.jsx
@@ -0,0 +1,13 @@
+import MoviesList from './../MoviesList/MoviesList.jsx'
+
+export default function MoviesCatalogue(props){
+    return(
+        <section>
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+        </section>
+    )
+}
---
+++ components/MoviesList/MoviesList.jsx
@@ -0,0 +1,18 @@
+export default function MoviesList(props){
+    return(
+        <div>
+            <h2>{props.genre}</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
