20683454196454e20ad99449585e589e9946ae7e
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-04-05 10:40:46 -0300

    Added 'key' property on 'MovieList' components

--- components/MoviesCatalogue/MoviesCatalogue.jsx
+++ components/MoviesCatalogue/MoviesCatalogue.jsx
@@ -2,9 +2,9 @@ import MoviesList from '../MoviesList/MoviesList.jsx'
 
 export default function MoviesCatalogue(props){
     const genresJson = props.genresJson;
-    const genresList = genresJson.genres.map(genreObj => {
+    const genresList = genresJson.genres.map((genreObj, index) => {
         const genreName = genreObj.name;
-        return <MoviesList genre={genreName} />
+        return <MoviesList key={index} genre={genreName} />
     })
     return(
         <section>
