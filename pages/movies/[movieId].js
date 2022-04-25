4824ec5129f8828e6a4dff28bd7224c32a564666
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-04-25 08:17:42 -0300

    Newly created page deleted
    It was made beacause I decided to change the way how to display more details for a single movie. I won't use a new page to do that.

--- pages/movies/[movieId].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Movie(){
-    const router = useRouter();
-    const { movieId } = router.query;
-
-    return <p>Movie: { movieId }</p>
-}
