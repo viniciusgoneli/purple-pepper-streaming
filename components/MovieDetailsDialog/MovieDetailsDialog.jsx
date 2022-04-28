2f57199f453effb6d9eae39090cc1c2df9592d92
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-04-28 10:25:23 -0300

    Implemented: hide body scrollY on open dialog
    This was made to not allow the page to be scrolled while the dialog is opened.

--- components/MovieDetailsDialog/MovieDetailsDialog.jsx
+++ components/MovieDetailsDialog/MovieDetailsDialog.jsx
@@ -3,21 +3,42 @@ import { useRef, useEffect } from 'react'
 
 export default function MovieDetailsDialog({ movie, closeDialog }){
 
-    const imageSrc = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
-
+    const body = document.querySelector('body');
     const dialogRoot = useRef(null);
 
+    const backdropImageSrc = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+
     useEffect(() => {
+        createDialogAtCurrentScrollPosition();
+        disableBodyScroll();
+    })
+
+    function createDialogAtCurrentScrollPosition(){
         dialogRoot.current.style.top = window.scrollY + 'px';
         dialogRoot.current.style.left = window.scrollX + 'px';
-    })
+    }
+
+    function disableBodyScroll(){
+        body.style.overflowY = 'hidden';
+    }
+
+    function enableBodyScroll(){
+        body.style.overflowY = 'auto';
+    }
+
+    function restaureDefaultAndCloseDialog(){
+        enableBodyScroll();
+        closeDialog();
+    }
 
     return(
         <div className={styles.dialogRoot} ref={dialogRoot}>
-            <div className={styles.darkBackground} onClick={_ => closeDialog()}></div>
+            <div className={styles.darkBackground} onClick={restaureDefaultAndCloseDialog}></div>
             <div className={styles.dialogBackground}>
-                <div className={styles.closeButton} onClick={_ => closeDialog()}><button>X</button></div>
-                <img className={styles.movieImage} src={imageSrc}/>
+                <div className={styles.closeButton} onClick={restaureDefaultAndCloseDialog}>
+                    <button>X</button>
+                </div>
+                <img className={styles.movieImage} src={backdropImageSrc}/>
                 <h1 className={styles.movieTitle}>{ movie.title }</h1>
                 <p className={styles.movieOverview}>{ movie.overview }</p>
                 <div className={styles.watchButtonContainer}>
