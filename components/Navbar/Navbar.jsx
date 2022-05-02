8ec57bdae221453ad1351083ee1dcdbb0273f6ee
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-05-02 10:59:30 -0300

    Implemented: 'SearchInput' jsx and css.
    It was done the logic to open/close the searchInput when the user to click on the searchButton.

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import styles from './../../styles/Navbar.module.css'
+import { useState } from 'react'
 
 export default function Navbar(){
+
+    const [isSearchInputOpen, setIsSearchInputOpen] = useState(false);
+
     return(
         <nav className={styles.navbar}>
             <div className={styles.leftContent}>
@@ -12,7 +16,16 @@ export default function Navbar(){
                 </ul>
             </div>
             <div className={styles.rightContent}>
-                <button className={`${styles.backgroundImage} ${styles.icon} ${styles.searchBtn}`}></button>
+                <button onClick={_ => setIsSearchInputOpen(!isSearchInputOpen)}
+                            className={`${styles.backgroundImage}
+                            ${styles.icon}
+                            ${styles.searchBtn}`} />
+                { 
+                    isSearchInputOpen ? 
+                    <div className={styles.searchInputContainer}>
+                        <input type={'text'} className={styles.searchInput} />
+                    </div> : null
+                }
                 <button className={`${styles.backgroundImage} ${styles.icon} ${styles.accountBtn}`}></button>
             </div>
         </nav>
